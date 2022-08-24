6cf1e6ea03499fe36c3d18b3e00db9827beab62c
colani-sudo <matsenjwacolani@gmail.com>
2022-08-25 03:42:56 +0800

    Leetcode AddTwoNumbers

---
+++ LeetcodeJava/src/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int cOut = 0;
+
+        while(l1 != null)
+        {
+
+            int sum = l1.val + l2.val + cOut;
+            cOut = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        return head.next;
+    }
+}
+
+
